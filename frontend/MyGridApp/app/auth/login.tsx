7e6d54628796a9c2c894fe58abecc8851626a937
TheoDuhautois <theoduhautois@gmail.com>
2025-09-29 15:53:45 +0200

    Feat: started implementing apple sign in

--- frontend/MyGridApp/app/auth/login.tsx
+++ frontend/MyGridApp/app/auth/login.tsx
@@ -3,13 +3,35 @@ import { MainText } from "@/components/widgets/MainText";
 import { TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { scopedI18n } from "@/translations/i18n";
+import * as AppleAuthentication from 'expo-apple-authentication';
 
 const t = scopedI18n('auth.login')
 
 export default function Login () {
 
-  function handleGoogle () {
+  async function handleGoogle () {
 
+  }
+
+  async function handleApple () {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+      
+      if (credential) {
+        console.log(credential)
+      }
+    } catch (e: any) {
+      if (e.code === 'ERR_REQUEST_CANCELED') {
+        // handle that the user canceled the sign-in flow
+      } else {
+        // handle other errors
+      }
+    }
   }
 
   return (
@@ -18,7 +40,7 @@ export default function Login () {
         <MainText>{t('signInGoogle')}</MainText>
         <AntDesign name="google" size={18} color="white" style={{ marginLeft: 8 }} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleGoogle} style={{borderWidth: 1, borderColor: 'white', padding: 10, borderRadius: 4, marginTop: 18, flexDirection: 'row'}}>
+      <TouchableOpacity onPress={handleApple} style={{borderWidth: 1, borderColor: 'white', padding: 10, borderRadius: 4, marginTop: 18, flexDirection: 'row'}}>
         <MainText>{t('signInApple')}</MainText>
         <AntDesign name="apple" size={18} color="white" style={{ marginLeft: 8}} />
       </TouchableOpacity>
