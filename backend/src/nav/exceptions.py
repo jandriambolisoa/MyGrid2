a220e58540cf4c4e6083c34372c2a9c647e071b4
jandriambolisoa <andriambolisoa.jeremy@outlook.fr>
2026-02-26 15:27:29 +0100

    fix: handle error when home_get_championships is called with no results yet

--- backend/src/nav/exceptions.py
+++ backend/src/nav/exceptions.py
@@ -2,9 +2,16 @@ from fastapi import HTTPException, status
 from jinja2 import Environment
 
 from backend.src.predictions.texts import *
+from backend.src.nav.texts import *
 
 class DriverNotRegisteredForSessionError(HTTPException):
     status_code = status.HTTP_404_NOT_FOUND
     def __init__(self, language: str = "en", **kwargs):
         super().__init__(self.status_code, **kwargs)
         self.detail = driver_not_registered_for_session_message[language]
+
+class ChampionshipLeaderboardNotAvailableError(HTTPException):
+    status_code = status.HTTP_404_NOT_FOUND
+    def __init__(self, language: str = "en", **kwargs):
+        super().__init__(self.status_code, **kwargs)
+        self.detail = championship_leaderboard_not_available_message[language]
