473826317a4bf6e5b31f89ab1da1b521f2012fa7
jandriambolisoa <andriambolisoa.jeremy@outlook.fr>
2026-02-26 17:04:53 +0100

    feat: add send_global_simple_email endpoint

--- backend/src/manager/router.py
+++ backend/src/manager/router.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from typing import Union
 
 import requests
@@ -7,13 +8,14 @@ from exponent_server_sdk import PushClient, PushMessage, PushServerError, Device
 from fastapi import APIRouter, Depends, status
 
 from backend.db.database import Database, get_db
-from backend.exceptions import ForbiddenAccessException
+from backend.exceptions import ForbiddenAccessException, MicroservicesAreOffException
 from backend.oauth2 import get_current_user
 from backend.config import settings as app_settings
 from backend.src.manager.constants import MAX_POOL_SIZE, MAX_PUSH_NOTIFICATIONS_ATTEMPTS, \
     PUSH_NOTIFICATION_BACKOFF_BASE, PUSH_NOTIFICATION_BACKOFF_CAP
-from backend.src.notifications.dependencies import get_all_push_tokens
-from backend.src.notifications.schemas import PushNotification
+from backend.src.notifications.dependencies import get_all_push_tokens, get_all_verified_emails
+from backend.src.notifications.schemas import PushNotification, SimpleEmail
+from backend.src.users.exceptions import NoUserFoundError
 from backend.src.users.privileges import is_user_moderator_or_admin
 from backend.src.users.schemas import UserSelf
 from backend.src.predictions.schemas import PredictionSession, PredictionScoreSession
@@ -93,3 +95,33 @@ async def send_global_push_notification(to_send: PushNotification, language: str
                 await asyncio.sleep(await exponential_backoff(attempt, PUSH_NOTIFICATION_BACKOFF_CAP, PUSH_NOTIFICATION_BACKOFF_BASE)) # Retry
 
         print("# PUSH NOTIFICATIONS: end")
+
+
+@router.post("/notification/mail/simple/send", status_code=status.HTTP_200_OK)
+async def send_global_simple_email(to_send: SimpleEmail, current_user: UserSelf = Depends(get_current_user)):
+    if not is_user_moderator_or_admin(current_user.id):
+        raise ForbiddenAccessException(language= "en")
+
+    if app_settings.ms == 0:
+        raise MicroservicesAreOffException()
+
+    emails = await get_all_verified_emails(MAX_POOL_SIZE)
+
+    if not emails:
+        raise NoUserFoundError(language="en")
+
+    for email in emails:
+        datas = {
+            "receiver": email.email,
+            "subject": to_send.subject.get(email.language, to_send.subject["en"]),
+            "fields": {
+                "PREVIEW_TEXT": to_send.preview.get(email.language, to_send.preview["en"]),
+                "IMAGE_URL": to_send.image_url,
+                "BODY": to_send.body.get(email.language, to_send.body["en"]),
+                "TITLE": to_send.title.get(email.language, to_send.title["en"])
+            }
+        }
+        try:
+            requests.post(f"{app_settings.ms_mailings_url}/sending/simple", data=json.dumps(datas))
+        except requests.exceptions.ConnectionError:
+            pass
--- backend/src/notifications/dependencies.py
+++ backend/src/notifications/dependencies.py
@@ -3,7 +3,7 @@ from typing import List
 from backend.db.database import get_db
 from backend.src.manager.constants import MAX_POOL_SIZE
 from backend.src.notifications.exceptions import NotAValidPushTokenError
-from backend.src.notifications.schemas import PushToken
+from backend.src.notifications.schemas import PushToken, UserEmail
 
 
 async def valid_push_token(token: str, language: str = "en") -> str:
@@ -24,3 +24,14 @@ async def get_all_push_tokens(limit: int = MAX_POOL_SIZE) -> List[PushToken]:
     tokens = db.cursor.fetchall()
 
     return [PushToken(**token) for token in tokens]
+
+async def get_all_verified_emails(limit: int = MAX_POOL_SIZE) -> List[UserEmail]:
+    db = get_db()
+    db.cursor.execute("""\
+        SELECT username, email, language
+        FROM users
+        WHERE verified = true
+        LIMIT %s""", (limit,))
+    emails = db.cursor.fetchall()
+
+    return [UserEmail(**email) for email in emails]
