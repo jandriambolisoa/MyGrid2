fd0dacf8a8c0087de1b7bd1e336b1a40b548d3ea
TheoDuhautois <theoduhautois@gmail.com>
2026-03-06 22:36:12 +0100

    feat: add change password

--- frontend/MyGridApp/components/widgets/buttons/BackButton.tsx
+++ frontend/MyGridApp/components/widgets/buttons/BackButton.tsx
@@ -1,16 +1,20 @@
 import { useRouter } from "expo-router"
-import { TouchableOpacity } from "react-native"
+import { TouchableOpacity, TouchableOpacityProps } from "react-native"
 import { Colors } from "@/theme"
 import { Feather } from "@expo/vector-icons"
 
-export function BackButton () {
+export function BackButton ({
+  style,
+  ...otherProps
+} : TouchableOpacityProps) {
 
   const router = useRouter()
 
   return (
     <TouchableOpacity
-      style={{ position: 'absolute', width: 60, left: 0, bottom: 0, top: 0, alignItems: 'center', justifyContent: 'center' }}
+      style={[{ position: 'absolute', width: 60, left: 0, bottom: 0, top: 0, alignItems: 'center', justifyContent: 'center' }, style]}
       onPress={router.back}
+      {...otherProps}
     >
       <Feather name="arrow-left" size={34} color={Colors.light.lightText}/>
     </TouchableOpacity>
