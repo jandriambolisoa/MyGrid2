2ea027e36d5b6c947bf4bb751a8158ed889ffbc1
jandriambolisoa <andriambolisoa.jeremy@outlook.fr>
2026-03-11 22:52:14 +0100

    feat: add home_get_one_event endpoint

--- backend/src/nav/router.py
+++ backend/src/nav/router.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter, Depends, status
 from backend.constants import QUERY_LIMIT
 from backend.db.database import Database, get_db
 from backend.oauth2 import get_current_user
-from backend.src.events.dependencies import valid_championship_id, get_upcoming_event,\
-    is_session_over, get_event_championship
+from backend.src.events.dependencies import valid_championship_id, get_upcoming_event, \
+    is_session_over, get_event_championship, valid_event_id, get_event_from_id
 from backend.src.nav.exceptions import ChampionshipLeaderboardNotAvailableError
 from backend.src.nav.schemas import NavMainEvent, NavMainEventSession, NavChampionship, NavChampionshipEvents, \
     DriverChampionshipLeaderboardWithPrediction, TeamChampionshipLeaderboardWithPrediction
@@ -235,6 +235,61 @@ async def home_get_events(championship_id: int = Depends(valid_championship_id),
         ], key=lambda event: event["datetime"])
     }
 
+@router.get("/home/events/{event_id}", response_model=NavMainEvent, status_code=status.HTTP_200_OK)
+async def home_get_one_event(event_id: int = Depends(valid_event_id), language: str = Depends(get_current_user_language), db: Database = Depends(get_db), current_user: UserSelf = Depends(get_current_user)):
+    event = await get_event_from_id(event_id, language)
+    championship = await get_event_championship(event.id, language)
+
+    db.cursor.execute("""
+        WITH sessions_translations AS (
+            SELECT session_id, name
+            FROM sessionstranslations
+            WHERE language = %s
+        )
+        SELECT sessions.id,
+        COALESCE(sessions_translations.name, sessions.name) AS name,
+        sessions.datetime,
+        sessions.competitive,
+        sessions.event_id
+        FROM sessions
+        LEFT JOIN sessions_translations ON sessions_translations.session_id = sessions.id
+        WHERE event_id = %s
+        ORDER BY datetime ASC""", (language, event.id))
+    sessions = db.cursor.fetchall()
+    sessions_to_return = list()
+
+    for session in sessions:
+        has_prono = await is_user_has_prono(current_user.id, session["id"])
+        nice_datetime = get_nice_datetime(session["datetime"], language)
+        is_over = await is_session_over(session["id"])
+
+        if is_over:
+            db.cursor.execute("""\
+                SELECT COALESCE(SUM(score), 0) AS total
+                FROM scores
+                WHERE user_id = %s
+                AND session_id = %s""", (current_user.id, session["id"]))
+            score_to_display = db.cursor.fetchone()
+        else:
+            db.cursor.execute("""\
+                SELECT COALESCE(SUM(potential), 0) AS total
+                FROM sessionspredictions
+                WHERE user_id = %s
+                AND session_id = %s""", (current_user.id, session["id"]))
+            score_to_display = db.cursor.fetchone()
+
+        session["has_prono"] = has_prono
+        session["nice_datetime"] = nice_datetime
+        session["is_over"] = is_over
+        session["score"] = score_to_display["total"]
+
+        sessions_to_return.append(NavMainEventSession(**session))
+
+    return {
+        "championship": championship,
+        "event": event,
+        "sessions": sessions_to_return
+    }
 
 @router.get("/standings/drivers", response_model=DriverChampionshipLeaderboardWithPrediction, status_code=status.HTTP_200_OK)
 async def get_championship_drivers_standings(championship_id: int = Depends(valid_championship_id), language: str = Depends(get_current_user_language), db: Database = Depends(get_db), current_user: UserSelf = Depends(get_current_user)):
