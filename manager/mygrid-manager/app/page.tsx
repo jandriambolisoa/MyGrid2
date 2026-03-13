6d09c0733081f46c5317d643638b8077fc9ab71b
TheoDuhautois <theoduhautois@gmail.com>
2026-03-13 12:19:16 +0100

    feat: created mygrid-manager + login page

---
+++ manager/mygrid-manager/app/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { useEmailLogin } from "@/hooks"
+
+export default function Home () {
+
+  const { emailLogin, error, loading } = useEmailLogin();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    if (error) {
+      setErrorMsg(error);
+    }
+  }, [error])
+
+  async function handleLogin () {
+
+    setErrorMsg('');
+
+    if (username.length === 0 || password.length === 0) {
+      setErrorMsg('Missing information');
+      return;
+    }
+
+    //const data = await emailLogin(username, password);
+
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Login</h1>
+      <input
+        className="input"
+        placeholder="Email or username"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        className="input"
+        style={{ marginBottom: 44 }}
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className="button" onClick={handleLogin}>Login</button>
+    </div>
+  )
+}
---
+++ manager/mygrid-manager/hooks/useEmailLogin.ts
@@ -0,0 +1,54 @@
+"use client"
+
+import { useState } from "react";
+
+const API_URL = process.env.EXPO_PUBLIC_API_URL
+
+export function useEmailLogin () {
+
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  async function emailLogin (username: string, password: string) {
+
+    const body = `${encodeURIComponent('username')}=${encodeURIComponent(username)}&${encodeURIComponent('password')}=${encodeURIComponent(password)}`
+
+    const options = {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: body
+    }
+
+    try {
+      setLoading(true);
+      setError(null);
+
+      const response = await fetch(`${API_URL}/auth/login-email`, options);
+
+      if (response.status >= 500) {
+        setError('SERVER');
+        return false;
+      }
+
+      const data = await response.json()
+
+      if (data.detail) {
+        setError(data.detail);
+        return false;
+      }
+
+      return data
+
+    } catch (e: any) {
+      setError(String(e.message));
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { emailLogin, error, loading };
+}
